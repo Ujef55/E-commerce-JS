3dab9c84fa32a6d096c7d8b0cc833443b22bb25b
Ujef <ujef.kelawala123@gmail.com>
2024-08-02 14:30:39 +0530

    add cart html structure and showCard js

---
+++ showCart.js
@@ -0,0 +1,43 @@
+import products from './api/products.json';
+import { getCartFromLocalStorage } from './getCartFromLocalStorage';
+
+const cartContainer = document.querySelector('#productCartContainer');
+const templateContainer = document.querySelector('#productCartTemplate');
+
+let localStorageProducts = getCartFromLocalStorage();
+
+let filteredProducts = products.filter((product) => {
+    return localStorageProducts.some((elem) => elem.id == product.id);
+});
+
+console.log(filteredProducts);
+
+filteredProducts.forEach((currentProduct) => {
+    let { name, image, price, category, id, stock } = currentProduct;
+
+    // get actual quantity and price from local storage
+    let localStorageData = getCartFromLocalStorage();
+    let existingProduct = localStorageData.find((product) => {
+        return product.id == id;
+    })
+    let quantity = 1;
+
+    if (existingProduct) {
+        quantity = existingProduct.quantity;
+        price = existingProduct.price;
+    }
+
+    const cloneProduct = document.importNode(templateContainer.content, true);
+
+    cloneProduct.querySelector('#cardValue').setAttribute('id', `card${id}`);
+    cloneProduct.querySelector('.category').innerText = name;
+    cloneProduct.querySelector('.productImage').setAttribute('src', image);
+    cloneProduct.querySelector('.productPrice').innerText = price;
+    cloneProduct.querySelector('.productQuantity').innerText = quantity;
+    cloneProduct.querySelector('.category').innerText = category;
+
+    cartContainer.appendChild(cloneProduct);
+})
+
+
+
